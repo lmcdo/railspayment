7e869cbf8d6b31ef3e24eff2d5d7a604240b3941
Nasir Sattar <lawrence.mcdonell@gmail.com>
2017-10-10 00:14:51 +1100

    email receipt to customer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :letter_opener
 
 
   # Enable/disable caching. By default caching is disabled.
